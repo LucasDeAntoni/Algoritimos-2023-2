ed07df6f2e4d8969f45e3cc79b6ddb8ccc8b27db
LucasDeAntoni <111450408+LucasDeAntoni@users.noreply.github.com>
2023-09-13 19:39:28 -0300

    Add files via upload

---
+++ PilhaLista.java
@@ -0,0 +1,43 @@
+package pilhas;
+
+public class PilhaLista <T> implements Pilha <T> {
+	
+	private ListaEncadeada<T> lista = new ListaEncadeada<T>();
+	
+
+	@Override
+	public void push(T valor) {
+		lista.inserir(valor);
+	}
+
+	@Override
+	public T pop() {
+		T valor;
+		valor = peek();
+		lista.retirar(valor);
+		return valor;
+	}
+
+	@Override
+	public T peek() {
+		if (estaVazia()) {
+			throw new PilhaVaziaException();
+		}
+		return lista.getPrimeiro().getInfo();
+	}
+
+	@Override
+	public boolean estaVazia() {
+		return lista.EstaVazia();
+	}
+
+	@Override
+	public void liberar() {
+		lista = new ListaEncadeada<>();
+		
+	}
+
+	public String toString() {
+		return lista.ToString();
+	}
+}
